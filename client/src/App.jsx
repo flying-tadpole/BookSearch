69ec5409b428f187fc81cac8e69bed8c9f3de89c
flyingtadpole <archer.curry@northwestern.edu>
2023-12-26 11:05:11 -0600

    updated app.jsx to work with Apollo/graphql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,47 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './App.css';
+
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  //the authLink allows us to communicate with graphQL
+  link: authLink.concat(httpLink),
+  //this also allows us to save some of the fetched data in the cache
+  cache: new InMemoryCache(),
+});
+
+//the apolloprovider allows all of the child components to access the queries
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 

86daedd21a60725c86b374e0802d1e725f1d7c45
flyingtadpole <archer.curry@northwestern.edu>
2023-12-26 11:01:49 -0600

    added front end mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,6 +14,39 @@ export const LOGIN_USER = gql`
 
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password)
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+  }
+`
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: BookInput!) {
+    saveBook(book: $book) {
+        _id
+        username
+        savedBooks {
+            bookId
+            title
+            authors
+            description
+        }
+    }
+  }
+`
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        savedBooks {
+            bookId
+        }
+    }
   }
 `
